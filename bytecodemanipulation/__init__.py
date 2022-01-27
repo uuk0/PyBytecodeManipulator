21cbc79ff900f367963dee9ef6b0d9487b42d273
uuk0 <baulukas1301@googlemail.com>
2022-01-27 10:35:41 +0100

    added __init__ code

--- bytecodemanipulation/__init__.py
+++ bytecodemanipulation/__init__.py
@@ -1,1 +1,5 @@
+from bytecodemanipulation import CodeOptimiser, Emulator, InstructionMatchers, MutableCodeObject, OptimiserAnnotations
+from bytecodemanipulation.TransformationHelper import BytecodePatchHelper
+from bytecodemanipulation.Transformers import TransformationHandler
+from bytecodemanipulation.util import Opcodes
 
