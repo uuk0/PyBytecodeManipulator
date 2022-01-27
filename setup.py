c53ea6e805e679c12c82d46a6fdc88e5efd3f909
uuk0 <baulukas1301@googlemail.com>
2022-01-27 10:26:12 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='bytecodemanipulation',
+   version='0.1.0',
+   description='High level API for python bytecode manipulation',
+   author='uuk',
+   author_email='uuk1301@gmail.com',
+   packages=['bytecodemanipulation'],  # same as name
+   install_requires=[],  # external packages as dependencies
+)
